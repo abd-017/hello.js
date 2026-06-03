9bbc06f9492c38dd94695eed2d658eec82a07503
abd-017 <abd290220@gmail.com>
2026-06-03 23:37:36 +0530

    Add files via upload

---
+++ abcd.js
@@ -0,0 +1,10 @@
+// 1. VARIABLE
+const name = "Abdullah";
+// 2. FUNCTION
+function greet(person) {
+return "Hello, " + person + "! Welcome to Nexora.";
+}
+// 3. LOOP
+const students = ["abd", "xyz", "lmn"];
+for (let i = 0; i < students.length; i++) {
+console.log( greet(students[i]) ); }
